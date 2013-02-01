fb3ea4316c77f4181cf08ac69f7d83202aacee8d
MichaelAz <michael.azarkevich@gmail.com>
2013-02-01 18:16:05 +0200

    First commit - tests are starting to form

---
+++ Tests/AmandaTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Tests
+{
+    [TestFixture]
+    class AmandaTests
+    {
+        [Test]
+        public static void Amanda_Should_Initialize()
+        {
+            
+        }
+
+
+        [Test]
+        public static void Amanda_Should_Expose_Method()
+        {
+            
+        }
+
+        [Test]
+        public static void Amanda_Should_Expose_Method_With_Route()
+        {
+            
+        }
+
+        [Test]
+        public static void Amanda_Should_Expose_Method_With_Verb()
+        {
+
+        }
+
+        [Test]
+        public static void Amanda_Should_Expose_Method_With_Handler()
+        {
+
+        }
+
+        [Test]
+        public static void Amanda_Should_Expose_Before_Handler()
+        {
+
+        }
+
+        [Test]
+        public static void Amanda_Should_Expose_After_Handler()
+        {
+
+        }
+
+        [Test]
+        public static void Amanda_Should_Expose_Class()
+        {
+
+        }
+    }
+}
