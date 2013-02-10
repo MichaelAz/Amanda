c1b8da6644f0aebed40decfceb71e371ce766dab
MichaelAz <michael.azarkevich@gmail.com>
2013-02-10 20:17:06 +0200

    Added Tests

--- Amanda_Client_Sample_Application/Program.cs
+++ Amanda_Client_Sample_Application/Program.cs
@@ -11,9 +11,19 @@ namespace Amanda_Client_Sample_Application
     {
         static void Main(string[] args)
         {
+            // Initialize an AmandaClient pointing to an Amanda webservice located at a given uri
             dynamic client = new AmandaClient("http://localhost:15534/api");
-            var res = client.SpliceUsers(new {ID = 1, Username = "shoopi", Password = "spliced"},
-                                         new {ID = 2, Username = "whoopi", Password = "pass"});
+
+            // To call a method through the API, just call it as you would locally
+            client.DoNothing();
+
+            // Methods that take complex parameters can be given anonymous types with
+            // the correct properties
+            var res = client.SpliceUsers(new {ID = 1, Username = "test", Password = "spliced"},
+                                         new {ID = 2, Username = "test2", Password = "pass"});
+
+            // Returned results are either a simple type like int or string or an ExpandoObject
+            // if the returned type is complex
             Console.WriteLine(res.ID);
             Console.WriteLine(res.Username);
             Console.WriteLine(res.Password);
---
+++ Amanda_Client_Sample_Application/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Amanda.Client;
+
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Amanda_Client_Sample_Application
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public static void Should_Succed_On_Exposed_Methods()
+        {
+            dynamic client = new AmandaClient("http://localhost:15534/api");
+
+            var actions = new Action[]
+                              {
+                                  () => client.DoNothing(),
+                                  () => client.DoNothingFor(1),
+                                  () => client.DoNothingForFactor(1.5, 2),
+                                  () => client.GetWastedTime(),
+                                  () => client.GetUsersUsername(new {ID = 1, Username = "test", Password = "pass"}),
+                                  () =>
+                                  client.SpliceUsers(new {ID = 1, Username = "test", Password = "pass"},
+                                                     new {ID = 2, Username = "test2", Password = "pass2"})
+                              };
+
+            foreach (var action in actions)
+            {
+                action.ShouldNotThrow();
+            }
+
+        }
+
+        [Test]
+        public static void Should_Verify_Simple_Params()
+        {
+            dynamic client = new AmandaClient("http://localhost:15534/api");
+
+            Action doNothingForFactor = () => client.DoNothingFor(1, 2.5);
+
+            doNothingForFactor.ShouldThrow<ArgumentException>();
+        }
+    }
+}
